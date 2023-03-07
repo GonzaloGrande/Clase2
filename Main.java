464d2cd7b17fffe2f75c4854bd34185b3ee64448
Gonzalo Grande <grandegonzalo92933@gmail.com>
2023-03-06 21:30:21 -0300

    commit con modificacion local y remotamente sin pull o push

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ public class Main
 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-         System.out.println("Le agregue un par de cosas");
+
+        System.out.println("Le agregue un par de cosas");
+
+        System.out.println("Le agregue localmente");
     }
 }
